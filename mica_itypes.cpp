6f179c09490194feb820809232d723d7e471933f
Yufeng <guyufeng0217@163.com>
2020-10-02 15:44:19 +0800

    print special instr & opcodes

--- mica_itypes.cpp
+++ mica_itypes.cpp
@@ -369,14 +369,14 @@ VOID instrument_itypes(INS ins, VOID* v){
 					if(group_identifiers[i][j].type == identifier_type::ID_TYPE_SPECIAL){
 						if(strcmp(group_identifiers[i][j].str, "mem_read") == 0 && INS_IsMemoryRead(ins) && strcmp(group_identifiers[12][0].str, cat) == 0){
 							INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)itypes_count, IARG_UINT32, i, IARG_END);
-							cout << "MemoryRead: " << cat << " ";
+							cout << "MemoryRead: " << cat << "\n";
 							categorized = true;
 							break;
 						}
 						else{
 							if(strcmp(group_identifiers[i][j].str, "mem_write") == 0 && INS_IsMemoryWrite(ins) && strcmp(group_identifiers[12][0].str, cat) == 0){
 								INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)itypes_count, IARG_UINT32, i, IARG_END);
-								cout << "MemoryWite: " << cat << " ";
+								cout << "MemoryWite: " << cat << "\n";
 								categorized = true;
 								break;
 							}
@@ -391,12 +391,11 @@ VOID instrument_itypes(INS ins, VOID* v){
 								}
 								if(flag==0)
 								    INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)itypes_count, IARG_UINT32, i, IARG_END);
-								cout << "Reg_transfer: " << cat << " ";
+								cout << "Reg_transfer: " << cat << "\n";
 							}
 							else{
 							}
 						}
-						cout << "\n";
 					}
 					else{
 						cerr << "ERROR! Unknown identifier type specified (" << group_identifiers[i][j].type << ")." << i << j << endl;
