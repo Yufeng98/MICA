733ec693395c68b558029f3bf5fdb1288b7f6b23
Yufeng <guyufeng0217@163.com>
2020-10-13 10:15:06 +0800

    bug for vector grouping

--- mica_itypes.cpp
+++ mica_itypes.cpp
@@ -73,7 +73,7 @@ VOID itypes_count(UINT32 gid){
 // initialize default groups
 VOID init_itypes_default_groups(){
 
-	number_of_groups = 23;
+	number_of_groups = 21;
 
 	group_identifiers = (identifier**)checked_malloc((number_of_groups+1)*sizeof(identifier*));
 	group_ids_cnt = (INT64*)checked_malloc((number_of_groups+1)*sizeof(INT64));
